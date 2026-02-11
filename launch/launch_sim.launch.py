2fdbe8b657935292b6849b436ae9e12893985f2e
senku <benomilo55@gmail.com>
2026-02-11 10:13:26 +0300

    gazebo sim fixed & lidar scanning added & mapping under work

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,69 +2,72 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
+
 
 
 def generate_launch_description():
 
-    package_name = 'my_bot'  # CHANGE IF NEEDED
 
-    bridge_config = os.path.join(
-        get_package_share_directory(package_name),
-        'config',
-        'gz_bridge.yaml'
-    )
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    # Robot State Publisher (URDF/Xacro)
+    package_name='my_bot' #<--- CHANGE ME
+
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory(package_name),
-                'launch',
-                'rsp.launch.py'
-            )
-        ),
-        launch_arguments={'use_sim_time': 'true'}.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+    
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value="empty.sdf",
+        description='World to load'
+        )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+             )
+
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
+                        output='screen')
+
+
+    # Launch the ROS-Gazebo bridge for normal topics
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
     )
 
-    # Gazebo Harmonic (gz sim)
-    gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('ros_gz_sim'),
-                'launch',
-                'gz_sim.launch.py'
-            )
-        ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
-    )
 
-    # Spawn robot from robot_description
-    spawn_robot = ExecuteProcess(
-        cmd=[
-            'ros2', 'run', 'ros_gz_sim', 'create',
-            '-name', 'my_bot',
-            '-topic', 'robot_description',
-            '-z', '0.063'
-        ],
-        output='screen'
-    )
 
-    # Bridge ROS <-> Gazebo topics (cmd_vel, odom, etc.)
-    gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        name='gz_bridge',
-        parameters=[{'config_file': bridge_config}],
-        output='screen'
-    )
-
+    # Launch them all!
     return LaunchDescription([
         rsp,
-        gz_sim,
-        spawn_robot,
-        gz_bridge
+        world_arg,
+        gazebo,
+        spawn_entity,
+        ros_gz_bridge,
     ])
